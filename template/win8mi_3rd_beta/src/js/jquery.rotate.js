aaa962b5c4388f0d67604a5b1fc2345e69156527
Takashiro <takashiro@qq.com>
2014-12-10 16:19:47 +0800

    Simplify jQuery rotate Do not support nonstandard tags of "transform"

--- template/win8mi_3rd_beta/src/js/jquery.rotate.js
+++ template/win8mi_3rd_beta/src/js/jquery.rotate.js
@@ -1,47 +1,25 @@
-/* 
+/*
  * JQuery CSS Rotate property using CSS3 Transformations
  * Copyright (c) 2011 Jakub Jankiewicz  <http://jcubic.pl>
  * licensed under the LGPL Version 3 license.
  * http://www.gnu.org/licenses/lgpl.html
  */
 (function($) {
-    function getTransformProperty(element) {
-        var properties = ['transform', 'WebkitTransform',
-                          'MozTransform', 'msTransform',
-                          'OTransform'];
-        var p;
-        while (p = properties.shift()) {
-            if (element.style[p] !== undefined) {
-                return p;
-            }
-        }
-        return false;
-    }
     $.cssHooks['rotate'] = {
-        get: function(elem, computed, extra){
-            var property = getTransformProperty(elem);
-            if (property) {
-                return elem.style[property].replace(/.*rotate\((.*)deg\).*/, '$1');
-            } else {
-                return '';
-            }
-        },
-        set: function(elem, value){
-            var property = getTransformProperty(elem);
-            if (property) {
-                value = parseInt(value);
-                $(elem).data('rotatation', value);
-                if (value == 0) {
-                    elem.style[property] = '';
-                } else {
-                    elem.style[property] = 'rotate(' + value%360 + 'deg)';
-                }
-            } else {
-                return '';
-            }
-        }
+		get: function(elem, computed, extra){
+		    return elem.style['transform'].replace(/.*rotate\((.*)deg\).*/, '$1');
+		},
+		set: function(elem, value){
+		    value = parseInt(value);
+			$(elem).data('rotatation', value);
+			if (value == 0) {
+				elem.style['transform'] = '';
+			} else {
+				elem.style['transform'] = 'rotate(' + value%360 + 'deg)';
+			}
+		}
     };
     $.fx.step['rotate'] = function(fx){
-        $.cssHooks['rotate'].set(fx.elem, fx.now);
+		$.cssHooks['rotate'].set(fx.elem, fx.now);
     };
 })(jQuery);
