9bdbf0e58f8b31e5b7a88f35b94cf3fad38bae20
Takashiro <takashiro@qq.com>
2014-12-12 22:53:57 +0800

    Rotate the logo automatically

--- template/win8mi_3rd_beta/src/js/nav.js
+++ template/win8mi_3rd_beta/src/js/nav.js
@@ -5,7 +5,27 @@
 		$(window).scroll(function(){
 			var angle = Math.floor($(window).scrollTop() / 3);
 			logo.css({'rotate':angle});
+			logo.data('rotate-time', $.now());
 		});
+
+		setInterval(function(){
+			var logo = $('.hd_logo').find('img');
+			if(logo.data('is_rotating')){
+				return;
+			}
+
+			var last_time = logo.data('rotate-time');
+			var now = $.now();
+			if (last_time == undefined || now - last_time >= 4000){
+				logo.data('is_rotating', true);
+				var angle = Math.floor($(window).scrollTop() / 3);
+				logo.animate({'rotate': angle + 359}, 500, function(){
+					$(this).css('rotate', angle);
+					logo.data('is_rotating', false);
+					logo.data('rotate-time', $.now());
+				});
+			}
+		}, 4000);
 
 		$('.nav a').mouseenter(function(){
 			var logo = $('.hd_logo').find('img');
