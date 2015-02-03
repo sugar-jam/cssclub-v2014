0d0d39588485df496816009bb6e8e86cf4c68851
Takashiro <takashiro@qq.com>
2015-02-03 19:31:47 +0800

    Forbid unverified member visiting portal pages

--- source/plugin/takashiro_portalperm/portalperm.class.php
+++ source/plugin/takashiro_portalperm/portalperm.class.php
@@ -16,13 +16,13 @@ class plugin_takashiro_portalperm_portal extends plugin_takashiro_portalperm {
 
 		//@todo: this should be a configuration
 		$forbidden_usergroup_table = array(
-			3 => array(6, 7, 8),
+			3 => array(6, 7, 8, 9, 20),
 		);
 
 		if(isset($forbidden_usergroup_table[$article['catid']])){
 			$forbidden_usergroups = $forbidden_usergroup_table[$article['catid']];
 			if(in_array($_G['groupid'], $forbidden_usergroups)){
-				if(in_array($_G['groupid'], array(6, 7, 8))){
+				if(in_array($_G['groupid'], array(6, 7, 8, 9))){
 					showmessage('先登录一下呀~', null, array(), array('showmsg' => true, 'login' => 1));
 				}else{
 					showmessage('抱歉，您没有权限访问该内容哦……');
