09f5625ffc5765c795e6a08a85b1cc58634d9da9
Takashiro <takashiro@qq.com>
2015-03-28 23:46:40 +0800

    Update copyright

--- template/iss_windmill/common/footer.php
+++ template/iss_windmill/common/footer.php
@@ -62,7 +62,7 @@
 			<li class="email"><a href="mailto:tech@issclub.org">tech@issclub.org</a></li>
 		</ul>
 		<div class="copyright">
-			<p>Powered by Discuz! <em>$_G['setting']['version']</em>, <!--{if !empty($_G['setting']['boardlicensed'])}--> <a href="http://license.comsenz.com/?pid=1&host=$_SERVER[HTTP_HOST]" target="_blank">Licensed</a><!--{/if}--> &copy; 2001-2013 <a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a> All rights reserved.</p>
+			<p>Powered by Discuz! <em>$_G['setting']['version']</em>, <!--{if !empty($_G['setting']['boardlicensed'])}--> <a href="http://license.comsenz.com/?pid=1&host=$_SERVER[HTTP_HOST]" target="_blank">Licensed</a><!--{/if}--> &copy; 2001-2013 <a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a> All rights reserved. Modified by Kazuichi Takashiro.</p>
 			<p>浙江大学启新团制作维护，<!--{if $_G['setting']['icp']}--> <a href="http://www.miitbeian.gov.cn/" target="_blank">$_G['setting']['icp']</a> <!--{/if}--></p>
 		</div>
 		<div class="extralink">
