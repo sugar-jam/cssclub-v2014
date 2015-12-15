e755cb426cecb8f3a805b39bef8f5191e5a354c5
Takashiro <takashiro@qq.com>
2015-12-15 14:08:52 +0800

    Refresh via ajax

--- source/plugin/takashiro_lovewins/template/js/common.js
+++ source/plugin/takashiro_lovewins/template/js/common.js
@@ -27,6 +27,32 @@
 		}, 'text');
 	});
 
+	function fill_userlist(users){
+		var listbox = $('#userlist');
+
+		if(users.length > 0){
+			listbox.html('');
+
+			for(var i = 0; i < users.length; i++){
+				var user = users[i];
+
+				var item = user_item.clone();
+				var avatar_link = item.find('.user .avatar a');
+				avatar_link.html(user.avatar);
+				avatar_link.attr('href', 'home.php?mod=space&uid=' + user.uid);
+
+				item.find('.realname').html(user.realname);
+				item.find('.issbranch').html(user.issbranch);
+				item.find('.briefintro').html(user.age + '岁 ' + user.constellation);
+				item.find('a.sendpm_button').attr('href', 'home.php?mod=spacecp&ac=pm&op=showmsg&handlekey=showmsg_' + user.uid + '&touid=' + user.uid + '&pmid=0&daterange=2');
+				item.data('uid', user.uid);
+
+				listbox.append(item);
+			}
+		}else{
+			alert('您要找的人不属于我们伟大的单身汪星球。');
+		}
+	}
 
 	var user_item = $('#userlist').children().eq(0).clone();
 	$('#search_user_form').on('submit', function(e){
@@ -39,85 +65,67 @@
 
 		input.val('');
 
-		$.post(root_url + '&action=search', {'keyword' : keyword}, function(users){
-			var listbox = $('#userlist');
+		$.post(root_url + '&action=search', {'keyword' : keyword}, fill_userlist, 'json');
+	});
 
-			if(users.length > 0){
-				listbox.html('');
-
-				for(var i = 0; i < users.length; i++){
-					var user = users[i];
-
-					var item = user_item.clone();
-					var avatar_link = item.find('.user .avatar a');
-					avatar_link.html(user.avatar);
-					avatar_link.attr('href', 'home.php?mod=space&uid=' + user.uid);
-
-					item.find('.realname').html(user.realname);
-					item.find('.issbranch').html(user.issbranch);
-					item.find('.briefintro').html(user.age + '岁 ' + user.constellation);
-					item.find('a.sendpm_button').attr('href', 'home.php?mod=spacecp&ac=pm&op=showmsg&handlekey=showmsg_' + user.uid + '&touid=' + user.uid + '&pmid=0&daterange=2');
-					item.data('uid', user.uid);
-
-					listbox.append(item);
-				}
-			}else{
-				alert('您要找的人不属于我们伟大的单身汪星球。');
-			}
-		}, 'json');
+	$('#refresh_userlist_button').on('click', function(){
+		$.post(root_url + '&action=search', {'randnum' : 12}, fill_userlist, 'json');
 	});
 
 	var couple_item = $('#couple_candidate_box').children().eq(0).clone();
+
+	function add_couple(user1, user2){
+		var item = couple_item.clone();
+
+		item.data('uid1', user1.uid1);
+		item.data('uid2', user2.uid2);
+
+		var avatars = item.find('.avatar');
+		var avatar1_link = avatars.eq(0).children();
+		avatar1_link.html(user1.avatar);
+		avatar1_link.attr('href', 'home.php?mod=space&uid=' + user1.uid);
+		var avatar2_link = avatars.eq(1).children();
+		avatar2_link.attr('href', 'home.php?mod=space&uid=' + user2.uid);
+		avatar2_link.html(user2.avatar);
+
+		var branch = item.find('.issbranch');
+		branch.eq(0).html(user1.issbranch);
+		branch.eq(1).html(user2.issbranch);
+
+		var realname = item.find('.realname');
+		realname.eq(0).html(user1.realname);
+		realname.eq(1).html(user2.realname);
+
+		$('#couple_candidate_box').append(item);
+	}
+
+	function generate_couple(users){
+		if(users.length < 2)
+			return;
+
+		if(users[0].length == 1 && users[1].length == 1){
+			$.post(root_url + '&action=votecouple', {'uid1' : users[0][0].uid, 'uid2' : users[1][0].uid}, function(response){
+				var response = parseInt(response, 10);
+				if(response == 1){
+					alert('成功支持这对CP！');
+				}else{
+					alert('您已经支持过他们啦！');
+				}
+			}, 'text');
+		}
+
+		$('#couple_candidate_box').html('');
+		for(var i = 0; i < users[0].length; i++){
+			for(var j = 0; j < users[1].length; j++){
+				add_couple(users[0][i], users[1][j]);
+			}
+		}
+	}
+
 	$('#search_couple_form').on('submit', function(e){
 		e.preventDefault();
 
 		var users = [];
-		function add_couple(){
-			if(users.length < 2)
-				return;
-
-			if(users[0].length == 1 && users[1].length == 1){
-				$.post(root_url + '&action=votecouple', {'uid1' : users[0][0].uid, 'uid2' : users[1][0].uid}, function(response){
-					var response = parseInt(response, 10);
-					if(response == 1){
-						alert('成功支持这对CP！');
-					}else{
-						alert('您已经支持过他们啦！');
-					}
-				}, 'text');
-			}
-
-			$('#couple_candidate_box').html('');
-			for(var i = 0; i < users[0].length; i++){
-				for(var j = 0; j < users[1].length; j++){
-					var item = couple_item.clone();
-					var user1 = users[0][i];
-					var user2 = users[1][j];
-
-					item.data('uid1', user1.uid1);
-					item.data('uid2', user2.uid2);
-
-					var avatars = item.find('.avatar');
-					var avatar1_link = avatars.eq(0).children();
-					avatar1_link.html(user1.avatar);
-					avatar1_link.attr('href', 'home.php?mod=space&uid=' + user1.uid);
-					var avatar2_link = avatars.eq(1).children();
-					avatar2_link.attr('href', 'home.php?mod=space&uid=' + user2.uid);
-					avatar2_link.html(user2.avatar);
-
-					var branch = item.find('.issbranch');
-					branch.eq(0).html(user1.issbranch);
-					branch.eq(1).html(user2.issbranch);
-
-					var realname = item.find('.realname');
-					realname.eq(0).html(user1.realname);
-					realname.eq(1).html(user2.realname);
-
-					$('#couple_candidate_box').append(item);
-				}
-			}
-		}
-
 		var inputs = [$('#couple1'), $('#couple2')];
 
 		if(inputs[0].val() == inputs[1].val()){
@@ -135,7 +143,33 @@
 			$.post(root_url + '&action=search', {'keyword' : keyword}, function(data){
 				if(data.length > 0){
 					users.push(data);
-					add_couple();
+					generate_couple(users);
+				}else{
+					alert('单身汪星球查无此人，真的有这个单身汪嘛？');
+				}
+			}, 'json');
+		}
+	});
+
+	$('#refresh_couplelist_button').on('click', function(){
+		var users = [];
+		var randnum = 6;
+
+		function generate_couple(){
+			if(users.length < 2)
+				return;
+
+			$('#couple_candidate_box').html('');
+			for(var i = 0; i < randnum; i++){
+				add_couple(users[0][i], users[1][i]);
+			}
+		}
+
+		for(var i = 1; i <= 2; i++){
+			$.post(root_url + '&action=search', {'randnum' : randnum, 'gender' : i}, function(data){
+				if(data.length > 0){
+					users.push(data);
+					generate_couple();
 				}else{
 					alert('单身汪星球查无此人，真的有这个单身汪嘛？');
 				}
