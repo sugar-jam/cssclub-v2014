05bbaf9e96a100de7d7acc6ed107e6b935b94eb5
Takashiro <takashiro@qq.com>
2017-04-16 00:43:30 +0800

    Fix name list

--- source/plugin/takashiro_issprofile/namelist.inc.php
+++ source/plugin/takashiro_issprofile/namelist.inc.php
@@ -4,7 +4,6 @@ if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
 	exit('Access Denied');
 }
 
-$verify_table = DB::table('plugin_member_verify');
-$members = DB::fetch_all("SELECT * FROM $verify_table WHERE uid IS NOT NULL");
+$members = DB::fetch_all("SELECT p.uid,p.realname,p.awardschool,p.awardyear FROM pre_common_member_profile p LEFT JOIN pre_common_member m ON m.uid=p.uid WHERE m.groupid NOT IN (8,19) AND realname!='' AND awardschool!='' AND awardyear!=''");
 
 include template('takashiro_issprofile:member_list');
